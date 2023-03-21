44214ed9667dc475955a8eea801005f758120de8
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-03-21 20:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ function promise(arr,time) {
 } 
 
 promise(arr,3000).then((res)=>{
-  console.log(res)
+
 	const evenArr = res.filter((num)=>{return num % 2 === 0});
 	return promise(evenArr,1000);
 }).then((res)=>{
-  console.log(res);
+const op = document.getElementById("output");
+	op.innerText=res;
 	const multiplyBy2 = res.map((num)=>{return num * 2});
+
 	return promise(multiplyBy2,2000);
+	
 }).then((res)=>{
-  console.log(res);
-	const op = document.getElementById("output");
+ 
+	
 	op.innerText=res;
 });
