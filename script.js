b64473bbf3ff34808849393e0b756b543711d6ee
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-03-21 20:12:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const arr = [1,2,3,4];
-function promise(arr,time) {
-	return new Promise((resolve) =>{
-		setTimeout(()=>{
-		resolve(arr);	
-		},time)
-	});
-} 
-
-promise(arr,3000).then((res)=>{
-
-	const evenArr = res.filter((num)=>{return num % 2 === 0});
-	return promise(evenArr,1000);
-}).then((res)=>{
+const arr = [1, 2, 3, 4];
+function promise(arr, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, time);
+  });
+}
 const op = document.getElementById("output");
-	op.innerText=res;
-	const multiplyBy2 = res.map((num)=>{return num * 2});
+promise(arr, 3000)
+  .then((res) => {
+    const evenArr = res.filter((num) => {
+      return num % 2 === 0;
+    });
+    return promise(evenArr, 1000);
+  })
+  .then((res) => {
+    
+    op.innerText = res;
+    const multiplyBy2 = res.map((num) => {
+      return num * 2;
+    });
 
-	return promise(multiplyBy2,2000);
-	
-}).then((res)=>{
- 
-	
-	op.innerText=res;
-});
+    return promise(multiplyBy2, 2000);
+  })
+  .then((res) => {
+    op.innerText = res;
+  });
